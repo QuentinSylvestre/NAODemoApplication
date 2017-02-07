4f9a161c1ed2213383ffaf7dccc783aa02fcd0e8
jchouki <jaouad.chouki@polestar.eu>
2017-02-07 09:47:18 +0100

    Version 4.3.4.1: 4.3.4.1- fix bug project iOS

--- Example/NAODemoApplication/IndoorLocationViewController.m
+++ Example/NAODemoApplication/IndoorLocationViewController.m
@@ -32,7 +32,6 @@
     
     if (self.locationHandle == nil) {
         [self startWaiting];
-        
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             dispatch_async(dispatch_get_main_queue(), ^{
                 self.locationHandle = [[NAOLocationHandle alloc] initWithKey:apiKey delegate:self sensorsDelegate:self];
